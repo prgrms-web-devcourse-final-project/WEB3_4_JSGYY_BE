4e93e94cbb865722dca3ae2b36bcf2031707dd5f
son <handsun000@gmail.com>
2025-03-24 15:41:44 +0900

    feat: mock chat

---
+++ src/main/kotlin/com/ll/nbe344team7/domain/chat/controller/ChatController.java
@@ -0,0 +1,52 @@
+package com.ll.nbe344team7.domain.chat.controller;
+
+import com.ll.nbe344team7.domain.chat.dto.ChatMessageDTO;
+import com.ll.nbe344team7.domain.chat.service.ChatService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+/**
+ * @author shjung
+ * @since 25. 3. 24.
+ */
+@RestController
+@RequestMapping("/api/chat/rooms")
+public class ChatController {
+
+    private final ChatService chatService;
+
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    @PostMapping("/${roomId}")
+    public ResponseEntity<?> sendMessage(
+            @RequestBody ChatMessageDTO chatMessageDTO,
+            @PathVariable long roomId
+    ) {
+        if (roomId == 10000)
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(Map.of("message", "채팅방을 찾을 수 없습니다."));
+
+        return ResponseEntity.ok(chatService.send(chatMessageDTO));
+    }
+
+    @GetMapping("/{roomId}")
+    public ResponseEntity<?> enterRoom(
+            @PathVariable long roomId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "") String message
+    ) {
+        if (roomId == 10000)
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(Map.of("message", "채팅방을 찾을 수 없습니다."));
+
+        return ResponseEntity.ok(chatService.items(roomId, message));
+    }
+}
