3b0cfe58e7956451cb07402b112b646a588fe43f
kjm722 <kjm722@naver.com>
2025-03-24 16:47:33 +0900

    feat : Mock chatroom 개발

---
+++ src/main/kotlin/com/ll/nbe344team7/domain/chatroom/service/ChatroomService.java
@@ -0,0 +1,34 @@
+package com.ll.nbe344team7.domain.chatroom.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ *
+ * @author kjm72
+ * @since 2025-03-24
+ */
+
+@Service
+public class ChatroomService {
+
+    public Map<Object, Object> createRoom(Long postId, Long sellerId, Long buyerId) {
+        return Map.of("message","채팅방 생성 성공");
+    }
+
+    public Map<Object,Object> listChatrooms(Long memberId){
+        List<Map<Object, Object>> rooms = List.of(
+                Map.of("id", 1, "nickname", "시계매니아"),
+                Map.of("id", 2, "nickname", "모아신발")
+        );
+
+        return Map.of("rooms",rooms);
+    }
+
+    public Map<Object,Object> deleteChatroom(Long roomId) {
+        return Map.of("message","채팅방 삭제 성공");
+    }
+}
