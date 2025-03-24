caed5f21e01ce44c04c19789ea258790e7432703
jsh4635 <49675179+jsh4635@users.noreply.github.com>
2025-03-24 16:13:31 +0900

    feat: mock account

---
+++ src/main/kotlin/com/ll/nbe344team7/domain/account/dto/ExchangeDTO.kt
@@ -0,0 +1,10 @@
+package com.ll.nbe344team7.domain.account.dto
+
+/**
+ *
+ *
+ *
+ * @author shjung
+ * @since 25. 3. 24.
+ */
+data class ExchangeDTO (val exchangeType: String, val payDate: String, val price: Long, val account: Long, val otherName: String)
