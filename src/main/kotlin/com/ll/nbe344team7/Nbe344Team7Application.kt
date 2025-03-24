6a8b8a3f977b1b98959f85b56eff6235aa16142f
jsh4635 <49675179+jsh4635@users.noreply.github.com>
2025-03-24 12:48:27 +0900

    setting

---
+++ src/main/kotlin/com/ll/nbe344team7/Nbe344Team7Application.kt
@@ -0,0 +1,11 @@
+package com.ll.nbe344team7
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Nbe344Team7Application
+
+fun main(args: Array<String>) {
+	runApplication<Nbe344Team7Application>(*args)
+}
---
+++ src/test/kotlin/com/ll/nbe344team7/Nbe344Team7ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ll.nbe344team7
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Nbe344Team7ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
