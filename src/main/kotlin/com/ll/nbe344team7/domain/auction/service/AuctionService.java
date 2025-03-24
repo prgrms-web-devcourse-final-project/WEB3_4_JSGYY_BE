3fe17a04a288eaeead4daa5f3857218e47dd02a1
jsh4635 <49675179+jsh4635@users.noreply.github.com>
2025-03-24 15:24:29 +0900

    feat: mock auction

---
+++ src/main/kotlin/com/ll/nbe344team7/domain/auction/service/AuctionService.java
@@ -0,0 +1,18 @@
+package com.ll.nbe344team7.domain.auction.service;
+
+import com.ll.nbe344team7.domain.auction.dto.BidDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * @author shjung
+ * @since 25. 3. 24.
+ */
+@Service
+public class AuctionService {
+
+    public Map<Object, Object> bidPrice(BidDTO dto, Long postId){
+        return Map.of("message", postId + "번 게시글 책 물품에 15000원 입찰이 완료되었습니다.");
+    }
+}
