28f067b4fd90dbe59033d54e3f3ee0f24a1961c2
GAEUN220 <gaeun2701@gmail.com>
2025-03-25 11:17:41 +0900

    feat: 게시글 작성 controller, service

--- src/main/kotlin/com/ll/nbe344team7/domain/post/repository/PostRepository.java
+++ src/main/kotlin/com/ll/nbe344team7/domain/post/repository/PostRepository.java
@@ -1,4 +1,9 @@
 package com.ll.nbe344team7.domain.post.repository;
 
-public interface PostRepository {
+import com.ll.nbe344team7.domain.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
