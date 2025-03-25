fa00f5a4d8880e4700e6f0b44559c6a905222413
jsh4635 <49675179+jsh4635@users.noreply.github.com>
2025-03-25 15:01:32 +0900

    feat: 경매 입찰 개발 진행

--- src/main/kotlin/com/ll/nbe344team7/domain/auction/repository/AuctionRepository.java
+++ src/main/kotlin/com/ll/nbe344team7/domain/auction/repository/AuctionRepository.java
@@ -4,11 +4,13 @@ import com.ll.nbe344team7.domain.auction.entity.Auction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * @author shjung
  * @since 25. 3. 25.
  */
 @Repository
 public interface AuctionRepository extends JpaRepository<Auction, Long> {
-    public Auction findByPostId(long postId);
+    public Optional<Auction> findByPostId(Long postId);
 }
--- src/main/kotlin/com/ll/nbe344team7/domain/auction/service/AuctionService.java
+++ src/main/kotlin/com/ll/nbe344team7/domain/auction/service/AuctionService.java
@@ -1,9 +1,12 @@
 package com.ll.nbe344team7.domain.auction.service;
 
 import com.ll.nbe344team7.domain.auction.dto.BidDTO;
+import com.ll.nbe344team7.domain.auction.entity.Auction;
+import com.ll.nbe344team7.domain.auction.repository.AuctionRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * @author shjung
@@ -12,10 +15,28 @@ import java.util.Map;
 @Service
 public class AuctionService {
 
+    private final AuctionRepository auctionRepository;
+
+    public AuctionService(AuctionRepository auctionRepository) {
+        this.auctionRepository = auctionRepository;
+    }
+
     public Map<Object, Object> bidPrice(BidDTO dto, Long postId){
+        Optional<Auction> op = auctionRepository.findByPostId(postId);
 
+        Auction auction = op.orElse(null);
+        if(auction == null){
+            throw new NullPointerException();
+        }
 
+        if(auction.getMaxPrice() < dto.getPrice()){
+            auction.setMaxPrice(dto.getPrice());
+            auction.setMemberId(dto.getMemberId());
+            auction = auctionRepository.save(auction);
+        } else if(auction.getMaxPrice() > dto.getPrice()){
+            throw new RuntimeException();
+        }
 
-        return Map.of("message", postId + "번 게시글 책 물품에 15000원 입찰이 완료되었습니다.");
+        return Map.of("message", postId + "번 게시글 물품에 " + auction.getMaxPrice() + "원 입찰이 완료되었습니다.");
     }
 }
