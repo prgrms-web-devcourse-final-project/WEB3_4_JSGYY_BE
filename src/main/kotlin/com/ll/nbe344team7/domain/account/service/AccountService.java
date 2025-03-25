2361ede4e515abe5d91f00794d6873dfc9313342
jsh4635 <49675179+jsh4635@users.noreply.github.com>
2025-03-25 11:35:55 +0900

    feat: repository add

--- src/main/kotlin/com/ll/nbe344team7/domain/account/service/AccountService.java
+++ src/main/kotlin/com/ll/nbe344team7/domain/account/service/AccountService.java
@@ -1,10 +1,7 @@
 package com.ll.nbe344team7.domain.account.service;
 
-import com.ll.nbe344team7.domain.account.dto.ExchangeDTO;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -38,15 +35,9 @@ public class AccountService {
      * @since 25. 3. 24.
      */
     public Map<Object, Object> getExchangeAccount(String exchangeType) {
-        List<ExchangeDTO> list = new ArrayList<>();
 
-        list.add(new ExchangeDTO("출금", "2025.03.25.", 15000,35000,"이광석"));
-        list.add(new ExchangeDTO("입금", "2025.03.26.", 50000,85000,"충전"));
-        list.add(new ExchangeDTO("출금", "2025.03.26.", 50000,35000,"김재민"));
-        list.add(new ExchangeDTO("출금", "2025.03.26.", 15000,20000,"박가은"));
-        list.add(new ExchangeDTO("입금", "2025.03.27.", 15000,35000,"손진영"));
-        list.add(new ExchangeDTO("출금", "2025.03.27.", 35000,0,"출금"));
 
-        return Map.of("exchanges", list);
+//        return Map.of("exchanges", list);
+        return null;
     }
 }
