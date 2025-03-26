3015e0857417a5cbcf2e3b7d31645534d38009f1
janjan <97529177+zxc8661@users.noreply.github.com>
2025-03-26 17:12:47 +0900

    주석 ì삭제

--- src/main/kotlin/com/ll/nbe344team7/domain/chatparticipant/entity/ChatParticipant.kt
+++ src/main/kotlin/com/ll/nbe344team7/domain/chatparticipant/entity/ChatParticipant.kt
@@ -1,27 +1,27 @@
-//package com.ll.nbe344team7.domain.chatparticipant.entity
-//
-//import com.ll.nbe344team7.domain.chatroom.entity.ChatRoom
-//import com.ll.nbe344team7.domain.member.Member
-//import com.ll.nbe344team7.global.base.BaseEntity
-//import jakarta.persistence.*
-//
-//@Entity
-//class ChatParticipant(
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "chatroom_id", nullable = false)
-//    val chatroom: ChatRoom,
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "member_id", nullable = false)
-//    val member: Member
-//) : BaseEntity() {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    var id : Long? = 0
-//
-//    companion object {
-//        fun create(chatroom: ChatRoom, member: Member): ChatParticipant {
-//            return ChatParticipant(chatroom = chatroom, member = member)
-//        }
-//    }
-//}
+package com.ll.nbe344team7.domain.chatparticipant.entity
+
+import com.ll.nbe344team7.domain.chatroom.entity.ChatRoom
+import com.ll.nbe344team7.domain.member.Member
+import com.ll.nbe344team7.global.base.BaseEntity
+import jakarta.persistence.*
+
+@Entity
+class ChatParticipant(
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chatroom_id", nullable = false)
+    val chatroom: ChatRoom,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    val member: Member
+) : BaseEntity() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id : Long? = 0
+
+    companion object {
+        fun create(chatroom: ChatRoom, member: Member): ChatParticipant {
+            return ChatParticipant(chatroom = chatroom, member = member)
+        }
+    }
+}
