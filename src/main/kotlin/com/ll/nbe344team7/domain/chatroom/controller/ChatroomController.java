24b3789164821d3fa4ce9beca409e0b1ae7ae920
janjan <97529177+zxc8661@users.noreply.github.com>
2025-03-26 15:12:48 +0900

    chore : ìchartroom 관련 주석처리

--- src/main/kotlin/com/ll/nbe344team7/domain/chatroom/controller/ChatroomController.java
+++ src/main/kotlin/com/ll/nbe344team7/domain/chatroom/controller/ChatroomController.java
@@ -1,33 +1,33 @@
-package com.ll.nbe344team7.domain.chatroom.controller;
-
-import com.ll.nbe344team7.domain.chatroom.dto.ChatRoomRequestDto;
-import com.ll.nbe344team7.domain.chatroom.service.ChatroomService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
-
-@RestController
-@RequestMapping("/api/chat/rooms")
-public class ChatroomController {
-
-    private final ChatroomService chatroomService;
-    public ChatroomController(ChatroomService chatroomService) {
-        this.chatroomService = chatroomService;
-    }
-
-    @PostMapping
-    public ResponseEntity<?> createChatroom(@RequestBody ChatRoomRequestDto requestDto) {
-        return ResponseEntity.ok(chatroomService.createRoom(requestDto));
-    }
-
-
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteChatroom(@PathVariable("id") Long roomId) {
-        if(roomId == 10000L){
-            return ResponseEntity.status(404).body(Map.of("message","해당 채팅방이 존재하지 않습니다."));
-        }
-        return ResponseEntity.ok(this.chatroomService.deleteChatroom(roomId));
-    }
-}
+//package com.ll.nbe344team7.domain.chatroom.controller;
+//
+//import com.ll.nbe344team7.domain.chatroom.dto.ChatRoomRequestDto;
+//import com.ll.nbe344team7.domain.chatroom.service.ChatroomService;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.Map;
+//
+//@RestController
+//@RequestMapping("/api/chat/rooms")
+//public class ChatroomController {
+//
+//    private final ChatroomService chatroomService;
+//    public ChatroomController(ChatroomService chatroomService) {
+//        this.chatroomService = chatroomService;
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity<?> createChatroom(@RequestBody ChatRoomRequestDto requestDto) {
+//        return ResponseEntity.ok(chatroomService.createRoom(requestDto));
+//    }
+//
+//
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<?> deleteChatroom(@PathVariable("id") Long roomId) {
+//        if(roomId == 10000L){
+//            return ResponseEntity.status(404).body(Map.of("message","해당 채팅방이 존재하지 않습니다."));
+//        }
+//        return ResponseEntity.ok(this.chatroomService.deleteChatroom(roomId));
+//    }
+//}
