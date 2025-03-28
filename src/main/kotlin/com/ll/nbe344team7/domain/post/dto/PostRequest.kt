cb94d84469ab5592920644cd341fe5b30fd43585
GAEUN220 <gaeun2701@gmail.com>
2025-03-28 09:48:27 +0900

    fix: Int -> Long

--- src/main/kotlin/com/ll/nbe344team7/domain/post/dto/PostRequest.kt
+++ src/main/kotlin/com/ll/nbe344team7/domain/post/dto/PostRequest.kt
@@ -3,7 +3,7 @@ package com.ll.nbe344team7.domain.post.dto
 data class PostRequest (
     val title: String,
     val content: String,
-    val price: Int,
+    val price: Long,
     val saleStatus: Boolean = true,
     val place: String,
     val auctionStatus: Boolean,
