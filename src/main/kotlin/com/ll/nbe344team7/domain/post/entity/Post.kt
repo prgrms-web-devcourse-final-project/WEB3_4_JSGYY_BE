0e87d627be8873b6baac3d2413b9f319e7f2b73c
GAEUN220 <gaeun2701@gmail.com>
2025-04-01 15:28:46 +0900

    fix: fetchType 설정

--- src/main/kotlin/com/ll/nbe344team7/domain/post/entity/Post.kt
+++ src/main/kotlin/com/ll/nbe344team7/domain/post/entity/Post.kt
@@ -56,7 +56,7 @@ class Post (
     var reports: Int = 0
         protected set
 
-    @OneToMany(mappedBy = "post", cascade = [CascadeType.REMOVE], orphanRemoval = true)
+    @OneToMany(mappedBy = "post", cascade = [CascadeType.REMOVE], fetch = FetchType.LAZY)
     var images: MutableList<ImageFile> = mutableListOf()
         protected set
 
