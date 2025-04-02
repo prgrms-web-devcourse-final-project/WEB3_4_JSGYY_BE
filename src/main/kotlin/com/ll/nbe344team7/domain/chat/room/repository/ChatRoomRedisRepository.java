2ce2596eae7f5ab448034d1e3d51b6de6334436d
kjm722 <kjm722@naver.com>
2025-04-02 22:14:25 +0900

    refactor : saveLastMessage member.Nickname으로 변경

--- src/main/kotlin/com/ll/nbe344team7/domain/chat/room/repository/ChatRoomRedisRepository.java
+++ src/main/kotlin/com/ll/nbe344team7/domain/chat/room/repository/ChatRoomRedisRepository.java
@@ -1,6 +1,10 @@
 package com.ll.nbe344team7.domain.chat.room.repository;
 
 import com.ll.nbe344team7.domain.chat.message.dto.MessageDTO;
+import com.ll.nbe344team7.domain.member.entity.Member;
+import com.ll.nbe344team7.domain.member.repository.MemberRepository;
+import com.ll.nbe344team7.global.exception.GlobalException;
+import com.ll.nbe344team7.global.exception.GlobalExceptionCode;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Repository;
@@ -17,10 +21,12 @@ import java.time.ZoneOffset;
 @Repository
 public class ChatRoomRedisRepository {
     private final RedisTemplate<String, String> redisTemplate;
+    private final MemberRepository memberRepository;
 
 
-    public ChatRoomRedisRepository(RedisTemplate<String, String> redisTemplate) {
+    public ChatRoomRedisRepository(RedisTemplate<String, String> redisTemplate, MemberRepository memberRepository) {
         this.redisTemplate = redisTemplate;
+        this.memberRepository = memberRepository;
     }
 
     private ZSetOperations<String, String> getZSetOperations() {
@@ -36,7 +42,8 @@ public class ChatRoomRedisRepository {
      * @since 2025-04-02
      */
     public void saveLastMessage(MessageDTO messageDTO, Long memberId) {
-        String value = messageDTO.getRoomId() + "|" +memberId + "|" + messageDTO.getContent() + "|" + LocalDateTime.now();
+        Member member= memberRepository.findById(memberId).orElseThrow(() -> new GlobalException(GlobalExceptionCode.NOT_FOUND_MEMBER));
+        String value = messageDTO.getRoomId() + "|" +member.getNickname() + "|" + messageDTO.getContent() + "|" + LocalDateTime.now();
         getZSetOperations().add("chatroom:"+messageDTO.getRoomId()+":lastMessage", value, LocalDateTime.now().toEpochSecond(ZoneOffset.UTC));
     }
 }
