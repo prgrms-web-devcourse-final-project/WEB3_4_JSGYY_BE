37d498d7b2586ea7d4a6d309fbeff6142c9b6d93
janjan <97529177+zxc8661@users.noreply.github.com>
2025-04-04 19:47:33 +0900

    some

--- src/main/kotlin/com/ll/nbe344team7/domain/member/controller/MemberController.java
+++ src/main/kotlin/com/ll/nbe344team7/domain/member/controller/MemberController.java
@@ -76,6 +76,7 @@ public class MemberController {
     @GetMapping("/member/mydetails")
     public ResponseEntity<Map<String, Object>> myDetails(
         @AuthenticationPrincipal    CustomUserDetails customUserDetails
+
     ){
         MemberDTO memberDTOS= memberService.myDetails(customUserDetails.getMemberId());
 
