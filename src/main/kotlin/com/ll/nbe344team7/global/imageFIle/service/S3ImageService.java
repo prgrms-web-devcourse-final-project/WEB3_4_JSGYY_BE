47e91abd497adccad25f70c7abfd89caeea06d61
GAEUN220 <gaeun2701@gmail.com>
2025-04-06 22:11:10 +0900

    fix: 최신버전

--- src/main/kotlin/com/ll/nbe344team7/global/imageFIle/service/S3ImageService.java
+++ src/main/kotlin/com/ll/nbe344team7/global/imageFIle/service/S3ImageService.java
@@ -1,119 +1,109 @@
 package com.ll.nbe344team7.global.imageFIle.service;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.util.IOUtils;
 import com.ll.nbe344team7.global.imageFIle.exception.S3Exception;
 import com.ll.nbe344team7.global.imageFIle.exception.S3ExceptionCode;
 import com.ll.nbe344team7.global.imageFIle.repository.ImageFileRepository;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 
-@Component
+@Service
 public class S3ImageService {
 
-    private final AmazonS3 amazonS3;
+    private final S3Client s3Client;
     private final ImageFileRepository imageFileRepository;
-
-    public S3ImageService(AmazonS3 amazonS3, ImageFileRepository imageFileRepository) {
-        this.amazonS3 = amazonS3;
-        this.imageFileRepository = imageFileRepository;
-    }
 
     @Value("${cloud.aws.s3.bucketName}")
     private String bucketName;
 
+    public S3ImageService(S3Client s3Client, ImageFileRepository imageFileRepository) {
+        this.s3Client = s3Client;
+        this.imageFileRepository = imageFileRepository;
+    }
+
     public String upload(MultipartFile image) {
-        if(image.isEmpty() || Objects.isNull(image.getOriginalFilename())){
+        if (image.isEmpty() || image.getOriginalFilename() == null) {
             throw new S3Exception(S3ExceptionCode.EMPTY_FILE_EXCEPTION);
         }
-        return this.uploadImage(image);
+        return uploadImage(image);
     }
 
     private String uploadImage(MultipartFile image) {
-        this.validateImageFileExtention(image.getOriginalFilename());
+        validateImageFileExtension(image.getOriginalFilename());
+
         try {
-            return this.uploadImageToS3(image);
+            return uploadImageToS3(image);
         } catch (IOException e) {
             throw new S3Exception(S3ExceptionCode.IO_EXCEPTION_ON_IMAGE_UPLOAD);
         }
     }
 
-    private void validateImageFileExtention(String filename) {
+    private void validateImageFileExtension(String filename) {
         int lastDotIndex = filename.lastIndexOf(".");
         if (lastDotIndex == -1) {
             throw new S3Exception(S3ExceptionCode.NO_FILE_EXTENTION);
         }
 
-        String extention = filename.substring(lastDotIndex + 1).toLowerCase();
-        List<String> allowedExtentionList = Arrays.asList("jpg", "jpeg", "png");
+        String extension = filename.substring(lastDotIndex + 1).toLowerCase();
+        List<String> allowedExtensions = Arrays.asList("jpg", "jpeg", "png");
 
-        if (!allowedExtentionList.contains(extention)) {
+        if (!allowedExtensions.contains(extension)) {
             throw new S3Exception(S3ExceptionCode.INVALID_FILE_EXTENTION);
         }
     }
 
     private String uploadImageToS3(MultipartFile image) throws IOException {
-        String originalFilename = image.getOriginalFilename(); //원본 파일 명
-        String extention = originalFilename.substring(originalFilename.lastIndexOf(".") + 1); //확장자 명
+        String originalFilename = image.getOriginalFilename();
+        String extension = originalFilename.substring(originalFilename.lastIndexOf(".") + 1).toLowerCase();
+        String s3FileName = UUID.randomUUID() + "_" + originalFilename;
 
-        String s3FileName = UUID.randomUUID() + "_" + image.getOriginalFilename();
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(s3FileName)
+                .contentType("image/" + extension)
+                .build();
 
-        InputStream is = image.getInputStream();
-        byte[] bytes = IOUtils.toByteArray(is);
-
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentType("image/" + extention);
-        metadata.setContentLength(bytes.length);
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
-
-        try{
-            PutObjectRequest putObjectRequest =
-                    new PutObjectRequest(bucketName, s3FileName, byteArrayInputStream, metadata);
-            amazonS3.putObject(putObjectRequest);
-        }catch (Exception e){
-            e.printStackTrace();
+        try {
+            s3Client.putObject(putObjectRequest, RequestBody.fromBytes(image.getBytes()));
+        } catch (Exception e) {
             throw new S3Exception(S3ExceptionCode.PUT_OBJECT_EXCEPTION);
-        }finally {
-            byteArrayInputStream.close();
-            is.close();
         }
 
-        return amazonS3.getUrl(bucketName, s3FileName).toString();
+        return "https://" + bucketName + ".s3.amazonaws.com/" + s3FileName;
     }
 
-    public void deleteImageFromS3(String imageAddress){
+    public void deleteImageFromS3(String imageAddress) {
         String key = getKeyFromImageAddress(imageAddress);
-        try{
-            amazonS3.deleteObject(new DeleteObjectRequest(bucketName, key));
-        }catch (Exception e){
+
+        try {
+            DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
+
+            s3Client.deleteObject(deleteObjectRequest);
+        } catch (Exception e) {
             throw new S3Exception(S3ExceptionCode.IO_EXCEPTION_ON_IMAGE_DELETE);
         }
     }
 
-    private String getKeyFromImageAddress(String imageAddress){
-        try{
-            URL url = new URL(imageAddress);
-            String decodingKey = URLDecoder.decode(url.getPath(), "UTF-8");
-            return decodingKey.substring(1); // 맨 앞의 '/' 제거
-        }catch (MalformedURLException | UnsupportedEncodingException e){
+    private String getKeyFromImageAddress(String imageAddress) {
+        try {
+            String decodedPath = URLDecoder.decode(imageAddress, StandardCharsets.UTF_8.name());
+            return decodedPath.substring(decodedPath.indexOf(".amazonaws.com/") + ".amazonaws.com/".length());
+        } catch (Exception e) {
             throw new S3Exception(S3ExceptionCode.IO_EXCEPTION_ON_IMAGE_DELETE);
         }
     }
 }
-
