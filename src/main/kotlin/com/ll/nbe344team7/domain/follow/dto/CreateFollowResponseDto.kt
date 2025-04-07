bd99191f0ef194833c3371fddd8f4b35e0246074
kjm722 <kjm722@naver.com>
2025-04-07 10:30:34 +0900

    refactor : DTO 기본 생성자 추가

--- src/main/kotlin/com/ll/nbe344team7/domain/follow/dto/CreateFollowResponseDto.kt
+++ src/main/kotlin/com/ll/nbe344team7/domain/follow/dto/CreateFollowResponseDto.kt
@@ -1,3 +1,5 @@
 package com.ll.nbe344team7.domain.follow.dto
 
-data class CreateFollowResponseDto(val message : String)
+data class CreateFollowResponseDto(val message : String){
+    constructor():this("")
+}
