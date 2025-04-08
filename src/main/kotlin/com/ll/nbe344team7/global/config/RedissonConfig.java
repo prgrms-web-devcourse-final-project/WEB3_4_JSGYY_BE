1546bc75ab36ed7672d3b8ff919ad31d063a1323
janjan <97529177+zxc8661@users.noreply.github.com>
2025-04-08 09:46:43 +0900

    redissonClient 수정

--- src/main/kotlin/com/ll/nbe344team7/global/config/RedissonConfig.java
+++ src/main/kotlin/com/ll/nbe344team7/global/config/RedissonConfig.java
@@ -3,17 +3,27 @@ package com.ll.nbe344team7.global.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 
 @Configuration
 public class RedissonConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
         config.useSingleServer()
-                .setAddress("redis://redis_1:6379")
+                .setAddress("redis://" + host + ":" + port)
+                .setPassword(password)
                 .setConnectionPoolSize(10)
                 .setConnectionMinimumIdleSize(5);
         return Redisson.create(config);
