2cb0de68407f5d2aac845ee9db97d49fd87c3a51
son <handsun000@gmail.com>
2025-04-11 17:07:20 +0900

    refactor: userDetails nickname 추가

--- src/main/kotlin/com/ll/nbe344team7/global/security/jwt/JWTFilter.java
+++ src/main/kotlin/com/ll/nbe344team7/global/security/jwt/JWTFilter.java
@@ -12,7 +12,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -26,8 +25,8 @@ import java.util.List;
 /**
  * jwt 를 검증 및 CustomUserDetails 저장하는  클래스
  *
- * @since 2025-03-26
  * @author 이광석
+ * @since 2025-03-26
  */
 public class JWTFilter extends OncePerRequestFilter {
 
@@ -50,9 +49,8 @@ public class JWTFilter extends OncePerRequestFilter {
      * @param filterChain
      * @throws ServletException
      * @throws IOException
-     *
-     * @since 2025-03-26
      * @author 이광석
+     * @since 2025-03-26
      */
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -63,94 +61,91 @@ public class JWTFilter extends OncePerRequestFilter {
 
         List<String> noCertifiedUrls = certifiedProperties.getNo();
 
-        for (String noCertifiedUrl : noCertifiedUrls){
-            if(request.getServletPath().contains(noCertifiedUrl)){
-                filterChain.doFilter(request,response);
+        for (String noCertifiedUrl : noCertifiedUrls) {
+            if (request.getServletPath().contains(noCertifiedUrl)) {
+                filterChain.doFilter(request, response);
                 return;
             }
         }
 
-       String accessToken = request.getHeader("access");
+        String accessToken = request.getHeader("access");
 
-       String refreshToken = getRefreshToken(request.getCookies());
+        String refreshToken = getRefreshToken(request.getCookies());
 
-      if(refreshToken==null){
-          throw new SecurityException(SecurityExceptionCode.TOKEN_NOT_EFFECTIVE);
-      }
-
-
-       //토큰 존재 확인
-       if(accessToken==null){
-           filterChain.doFilter(request,response);
-           return;
-       }
-
-       // 토큰 만료 확인
-       try{
-           jwtUtil.isExpired(accessToken);
-       }catch (ExpiredJwtException e){
-           throw new SecurityException(SecurityExceptionCode.ACCESSTOKEN_IS_EXPIRED);
-       }
-
-       // 토큰 종류 확인
-       if(!jwtUtil.getCategory(accessToken).equals("access")){
-           throw new SecurityException(SecurityExceptionCode.NOT_ACCESSTOKEN);
-       }
-
-       String redisAccessToken = redisRepository.get(refreshToken);
-
-       //Db와 비교
-       if(!accessToken.equals(redisAccessToken)){
-           redisRepository.delete(refreshToken);
-           throw new SecurityException(SecurityExceptionCode.TOKEN_MISMATCH);
-       }
-
-
-           String username = jwtUtil.getUsername(accessToken);
-           Long memberId = jwtUtil.getMemberId(accessToken);
-           String role = jwtUtil.getRole(accessToken);
-
-
-        if(username==null || memberId == null || role ==null){
+        if (refreshToken == null) {
             throw new SecurityException(SecurityExceptionCode.TOKEN_NOT_EFFECTIVE);
         }
 
 
+        //토큰 존재 확인
+        if (accessToken == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 토큰 만료 확인
+        try {
+            jwtUtil.isExpired(accessToken);
+        } catch (ExpiredJwtException e) {
+            throw new SecurityException(SecurityExceptionCode.ACCESSTOKEN_IS_EXPIRED);
+        }
+
+        // 토큰 종류 확인
+        if (!jwtUtil.getCategory(accessToken).equals("access")) {
+            throw new SecurityException(SecurityExceptionCode.NOT_ACCESSTOKEN);
+        }
+
+        String redisAccessToken = redisRepository.get(refreshToken);
+
+        //Db와 비교
+        if (!accessToken.equals(redisAccessToken)) {
+            redisRepository.delete(refreshToken);
+            throw new SecurityException(SecurityExceptionCode.TOKEN_MISMATCH);
+        }
 
 
+        String username = jwtUtil.getUsername(accessToken);
+        Long memberId = jwtUtil.getMemberId(accessToken);
+        String role = jwtUtil.getRole(accessToken);
+        String nickname = jwtUtil.getNickname(accessToken);
 
-        CustomUserData customUserData = new CustomUserData(memberId,username,role,"tmp");
+
+        if (username == null || memberId == null || role == null) {
+            throw new SecurityException(SecurityExceptionCode.TOKEN_NOT_EFFECTIVE);
+        }
+
+
+        CustomUserData customUserData = new CustomUserData(memberId, username, nickname, role, "tmp");
 
         CustomUserDetails customUserDetails = new CustomUserDetails(customUserData);
 
-        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails,null,customUserDetails.getAuthorities());
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
 
         SecurityContextHolder.getContext().setAuthentication(authToken);
 
-        filterChain.doFilter(request,response);
+        filterChain.doFilter(request, response);
 
     }
 
 
     /**
      * refresh 토큰 추출 메소드
+     *
      * @param cookies
      * @return
      * @author 이광석
      * @since 2025-03-28
      */
-    private String getRefreshToken(Cookie[] cookies){
+    private String getRefreshToken(Cookie[] cookies) {
 
-        for(Cookie cookie: cookies){
-            if(cookie.getName().equals("refresh")){
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("refresh")) {
                 return cookie.getValue();
             }
         }
 
         throw new SecurityException(SecurityExceptionCode.NOT_FOUND_REFRESHTOKEN);
     }
-
-
 
 
 }
