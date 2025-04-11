0950f16f9a3f9ffa50ad26704d64e606f97f9ed4
kjm722 <kjm722@naver.com>
2025-04-11 15:08:47 +0900

    refactor : FollowController 문서화

--- src/main/kotlin/com/ll/nbe344team7/domain/follow/dto/FollowRequestDto.kt
+++ src/main/kotlin/com/ll/nbe344team7/domain/follow/dto/FollowRequestDto.kt
@@ -1,5 +1,7 @@
 package com.ll.nbe344team7.domain.follow.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 /**
  * Follow request dto
  *
@@ -7,6 +9,10 @@ package com.ll.nbe344team7.domain.follow.dto
  * @property followingId
  * @constructor Create empty Follow request dto
  */
-data class FollowRequestDto(val followingId : Long){
+@Schema(description = "팔로우/언팔로우 요청 API")
+data class FollowRequestDto(
+    @field: Schema(description = "팔로우/언팔로우할 유저 ID", example = "2")
+    val followingId : Long
+){
     constructor():this(0)
 }
