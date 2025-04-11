d339e37295dd073c75989019d90c07c51d165905
kjm722 <kjm722@naver.com>
2025-04-11 15:07:30 +0900

    refactor : AuctionController 문서화

--- src/main/kotlin/com/ll/nbe344team7/domain/auction/controller/AuctionController.java
+++ src/main/kotlin/com/ll/nbe344team7/domain/auction/controller/AuctionController.java
@@ -4,6 +4,11 @@ import com.ll.nbe344team7.domain.auction.dto.BidDTO;
 import com.ll.nbe344team7.domain.auction.service.AuctionService;
 import com.ll.nbe344team7.global.security.dto.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -35,7 +40,33 @@ public class AuctionController {
      * @author shjung
      * @since 25. 3. 25.
      */
-    @Operation(summary = "입찰 기능")
+    @Operation(
+            summary = "입찰 기능",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "입찰 DTD",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = BidDTO.class)
+                    )
+            ),
+            parameters = {
+                    @Parameter(name = "postId", description = "게시글 ID", required = true)
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "입찰 성공", content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    name = "성공 응답 예시",
+                                    value = """
+                                            {
+                                                "message" : "1번 게시글 물품에 20000원 입찰이 완료되었습니다.
+                                            }
+                                            """
+                            )
+                    ))
+            }
+    )
     @PostMapping("/{postId}/bid")
     public ResponseEntity<?> bidPrice(@RequestBody BidDTO bidDTO, @PathVariable Long postId,
                                       @AuthenticationPrincipal CustomUserDetails userDetails) {
