b0a444c2a922131717114453db453c25fee6333b
son <handsun000@gmail.com>
2025-04-11 09:14:54 +0900

    feat: MessageSearchDTO 기본값

--- src/main/kotlin/com/ll/nbe344team7/domain/chat/message/dto/MessageSearchDTO.kt
+++ src/main/kotlin/com/ll/nbe344team7/domain/chat/message/dto/MessageSearchDTO.kt
@@ -7,6 +7,4 @@ package com.ll.nbe344team7.domain.chat.message.dto
  * @author jyson
  * @since 25. 3. 24.
  */
-data class MessageSearchDTO(var message: String, var page: Int, var size: Int){
-    constructor():this("",0,10)
-}
+data class MessageSearchDTO(var message: String = "", var page: Int = 0, var size: Int = 10)
