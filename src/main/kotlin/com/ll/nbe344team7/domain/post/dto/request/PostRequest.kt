8aa212eb6a461120d667e2cda415358fc0543dff
kjm722 <kjm722@naver.com>
2025-04-12 16:26:48 +0900

    refactor : PostController 문서화 완료(PostImageController는 추후에 작업 예정)

--- src/main/kotlin/com/ll/nbe344team7/domain/post/dto/request/PostRequest.kt
+++ src/main/kotlin/com/ll/nbe344team7/domain/post/dto/request/PostRequest.kt
@@ -1,12 +1,23 @@
 package com.ll.nbe344team7.domain.post.dto.request
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(name = "게시글 작성 DTO")
 data class PostRequest (
+    @field:Schema(description = "제목", required = true, example = "바지")
     val title: String = "",
+    @field:Schema(description = "내용", required = true, example = "청바지")
     val content: String = "",
+    @field:Schema(description = "가격", required = true, example = "20000")
     val price: Long = 0L,
+    @field:Schema(description = "카테고리", required = true, example = "남성의류")
     val category: String = "",
+    @field:Schema(description = "장소", required = true, example = "서울시 관악구")
     val place: String = "",
+    @field:Schema(description = "판매 상태", example = "true")
     val saleStatus: Boolean = true,
+    @field:Schema(description = "경매 여부", example = "false")
     val auctionStatus: Boolean = false,
+    @field:Schema(description = "경매 시작 시간과 종료시간(경매 여부가 true일 경우 작성 아니면 null로 입력)", example = "\"startedAt\":\"2025-03-27T10:00:00\",\"closedAt\":\"2025-04-10T10:00:00\"")
     val auctionRequest: AuctionRequest? = null
 )
--- src/main/kotlin/com/ll/nbe344team7/domain/post/dto/request/ReportRequest.kt
+++ src/main/kotlin/com/ll/nbe344team7/domain/post/dto/request/ReportRequest.kt
@@ -1,9 +1,16 @@
 package com.ll.nbe344team7.domain.post.dto.request
 
 import com.ll.nbe344team7.domain.post.entity.ReportType
+import io.swagger.v3.oas.annotations.media.Schema
 
+@Schema(name = "게시글 신고 DTO")
 data class ReportRequest(
+    @field:Schema(description = "신고 제목", example = "사기꾼 잡아주세요")
     val title: String = "",
+    @field:Schema(description = "신고 내용", example = "사기를 당했어요")
     val content: String = "",
+    @field:Schema(description = "신고 유형", example = "기타")
     val type: ReportType = ReportType.ETC
-)
+){
+    constructor(): this("","",ReportType.ETC)
+}
