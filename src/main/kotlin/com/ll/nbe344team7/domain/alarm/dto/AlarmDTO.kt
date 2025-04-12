be89da61eda8d74990652d2c8bdcc12beb612b4f
kjm722 <kjm722@naver.com>
2025-04-12 17:52:08 +0900

    refactor : AlarmController 문서화

--- src/main/kotlin/com/ll/nbe344team7/domain/alarm/dto/AlarmDTO.kt
+++ src/main/kotlin/com/ll/nbe344team7/domain/alarm/dto/AlarmDTO.kt
@@ -1,8 +1,7 @@
 package com.ll.nbe344team7.domain.alarm.dto
 
 import com.ll.nbe344team7.domain.alarm.entity.Alarm
-import com.ll.nbe344team7.domain.member.entity.Member
-import software.amazon.awssdk.services.s3.model.Destination
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDateTime
 
 /**
@@ -12,12 +11,19 @@ import java.time.LocalDateTime
  * @author jyson
  * @since 25. 3. 24.
  */
+@Schema(description = "알람 DTO")
 data class AlarmDTO(
+    @field:Schema(description = "알람 ID", example = "1")
     val id : Long?,
+    @field:Schema(description = "받는 사용자 ID", example = "1")
     val receiveMemberId : Long,
+    @field:Schema(description = "알람 내용", example = "낙찰되었습니다.")
     val content: String,
+    @field:Schema(description = "알림 타입", example = "1")
     val type: Int,
+    @field:Schema(description = "알림 확인", example = "false")
     val checked: Boolean?,
+    @field:Schema(description = "생성 날짜" , example = "")
     val createdAt: LocalDateTime?,
     val destinationId : Long)
 {
