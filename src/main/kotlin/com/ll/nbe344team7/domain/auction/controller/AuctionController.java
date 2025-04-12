7b06272fc42a5d26bbb3387130779aa2cc603480
kjm722 <kjm722@naver.com>
2025-04-12 17:11:40 +0900

    refactor : AuctionController 문서화 수정

--- src/main/kotlin/com/ll/nbe344team7/domain/auction/controller/AuctionController.java
+++ src/main/kotlin/com/ll/nbe344team7/domain/auction/controller/AuctionController.java
@@ -4,7 +4,6 @@ import com.ll.nbe344team7.domain.auction.dto.BidDTO;
 import com.ll.nbe344team7.domain.auction.service.AuctionService;
 import com.ll.nbe344team7.global.security.dto.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -42,10 +41,14 @@ public class AuctionController {
      */
     @Operation(
             summary = "입찰 기능",
-            parameters = {
-                    @Parameter(name = "postId", description = "게시글 ID", required = true, schema = @Schema(type = "integer")),
-                    @Parameter(name = "price", description = "입찰 금액", required = true, schema = @Schema(type = "integer"))
-            },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "입찰 DTO",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = BidDTO.class)
+                    )
+            ),
             responses = {
                     @ApiResponse(responseCode = "200", description = "입찰 성공", content = @Content(
                             mediaType = "application/json",
@@ -57,7 +60,38 @@ public class AuctionController {
                                             }
                                             """
                             )
+                    )),
+                    @ApiResponse(responseCode = "404", description = "게시글 조회 실패", content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = """
+                                            {
+                                                "message" : "게시글이 조회되지 않습니다."
+                                            }
+                                            """
+                            )
+                    )),
+                    @ApiResponse(responseCode = "404", description = "맴버 조회 실패", content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = """
+                                            {
+                                                "message" : "멤버가 조회되지 않습니다."
+                                            }
+                                            """
+                            )
+                    )),
+                    @ApiResponse(responseCode = "404", description = "입찰금 초과 오류", content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = """
+                                            {
+                                                "message" : "보유금보다 높은 입찰가를 입찰할 수는 없습니다."
+                                            }
+                                            """
+                            )
                     ))
+
             }
     )
     @PostMapping("/{postId}/bid")
