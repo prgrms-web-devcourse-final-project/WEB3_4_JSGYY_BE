168322de6b36c47431e4920a45bd1004d0bc96d7
janjan <97529177+zxc8661@users.noreply.github.com>
2025-04-14 12:09:16 +0900

    work

--- src/main/kotlin/com/ll/nbe344team7/global/security/jwt/LogoutFilter.java
+++ src/main/kotlin/com/ll/nbe344team7/global/security/jwt/LogoutFilter.java
@@ -24,12 +24,10 @@ public class LogoutFilter extends OncePerRequestFilter {
     private final JWTUtil jwtUtil;
     private final RedisRepository redisRepository;
 
-    private final MemberService memberService;
 
-    public LogoutFilter(JWTUtil jwtUtil, RedisRepository redisRepository, MemberService memberService) {
+    public LogoutFilter(JWTUtil jwtUtil, RedisRepository redisRepository) {
         this.jwtUtil = jwtUtil;
         this.redisRepository = redisRepository;
-        this.memberService = memberService;
     }
 
     /**
@@ -55,9 +53,7 @@ public class LogoutFilter extends OncePerRequestFilter {
             return;
         }
 
-        if(request.getRequestURI().equals("/api/auth/logout")){
-            memberService.withdrawal();
-        }
+
 
         // 쿠키에서 refresh 토큰 꺼내기
         String refresh = null;
