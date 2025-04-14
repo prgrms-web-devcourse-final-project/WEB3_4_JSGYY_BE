fc0d07cec006865f145638f5c444332d0d79c9cf
jsh4635 <49675179+jsh4635@users.noreply.github.com>
2025-04-14 23:44:52 +0900

    work

--- src/main/kotlin/com/ll/nbe344team7/domain/pay/dto/DepositDTO.kt
+++ src/main/kotlin/com/ll/nbe344team7/domain/pay/dto/DepositDTO.kt
@@ -13,12 +13,8 @@ import io.swagger.v3.oas.annotations.media.Schema
 data class DepositDTO(
     @field:Schema(description = "충전 금액", example = "50000")
     val price: Long?,
-    @field:Schema(description = "이름", example = "tmp")
-    val name: String,
-    @field:Schema(description = "은행", example = "신한은행")
-    val bank: String,
     @field:Schema(description = "UID", example = "imp-12345678")
     val impUid: String
 ){
-    constructor() : this(0, "", "", "")
+    constructor() : this(0, "")
 }
